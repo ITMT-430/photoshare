071712f7a129b1051f7114f1b3368a71ebd018d0
csiron <csiron@users.noreply.github.com>
2016-05-02 01:34:21 -0500

    uncommented lines

--- infrastructure/db-env.sh
+++ infrastructure/db-env.sh
@@ -13,7 +13,7 @@ sudo apt-get install -y git php-curl mysql-client curl vim
 
 git clone https://github.com/ITMT-430/photoshare.git
 
-#export DEBIAN_FRONTEND=noninteractive
-#sudo -E apt-get -q -y install mysql-server
+export DEBIAN_FRONTEND=noninteractive
+sudo -E apt-get -q -y install mysql-server
 
 mysql -u user -p < photoshare/php/127_0_0_1.sql
