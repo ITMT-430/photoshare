b93ccc15eb0f81a48834827020e4765f846a67b5
csiron <csiron@users.noreply.github.com>
2016-05-02 22:34:54 -0500

    removed php7 stuff, added .sql line

--- infrastructure/db-env.sh
+++ infrastructure/db-env.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 
-sudo apt-get install software-properties-common -y 
-sudo apt-get install python-software-properties -y
-sudo LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php -y
+export DEBIAN_FRONTEND=noninteractive
 sudo apt-get update -y
-sudo apt-get purge php5-common -y
-sudo apt-get install php7.0 php7.0-fpm php7.0-mysql -y
-sudo apt-get --purge autoremove -y
-
-sudo apt-get update -y
-sudo apt-get install -y git php-curl mysql-client curl vim
+apache2 git php5 php5-curl mysql-client curl vim php5-mysql
+sudo -E apt-get -q -y install mysql-server
 
 git clone https://github.com/ITMT-430/photoshare.git
 
-export DEBIAN_FRONTEND=noninteractive
-sudo -E apt-get -q -y install mysql-server
-
 mysql -u user -p < photoshare/php/127_0_0_1.sql
+
+mysql -u user -p < photoshare_430.sql
